07a3b4ebd6c86277e254b7aa9fcdc937a8b07321
Jim Fulton <jim@zope.com>
2016-12-01 10:24:45 -0500

    bug fixes

--- content_text.sql
+++ content_text.sql
@@ -1,5 +1,4 @@
-
-create or replace function content_text(class_name varchar, state jsonb)
+create or replace function content_text(class_name varchar, state_ jsonb)
   returns tsvector as $$
 declare
   title varchar;
@@ -11,47 +10,47 @@ declare
 begin
   if class_name = 'karl.models.profile.Profile' then
     text :=
-      coalesce(state #>> '{"__name__"}', '')
-      || ' ' || coalesce(state #>> '{"firstname"}', '')
-      || ' ' || coalesce(state #>> '{"lastname"}', '')
-      || ' ' || coalesce(state #>> '{"email"}', '')
-      || ' ' || coalesce(state #>> '{"phone"}', '')
-      || ' ' || coalesce(state #>> '{"extension"}', '')
-      || ' ' || coalesce(state #>> '{"department"}', '')
-      || ' ' || coalesce(state #>> '{"position"}', '')
-      || ' ' || coalesce(state #>> '{"organization"}', '')
-      || ' ' || coalesce(state #>> '{"location"}', '')
-      || ' ' || coalesce(state #>> '{"country"}', '')
-      || ' ' || coalesce(state #>> '{"website"}', '')
-      || ' ' || coalesce(state #>> '{"languages"}', '')
-      || ' ' || coalesce(state #>> '{"office"}', '')
-      || ' ' || coalesce(state #>> '{"room_no"}', '')
-      || ' ' || coalesce(state #>> '{"biography"}', '');
-  elseif class_name = 'karl.content.interfaces.ICommunityFile' then
-    hoid := state #>> '{"_extracted_data", "id", 1}';
+      coalesce(state_ #>> '{"__name__"}', '')
+      || ' ' || coalesce(state_ #>> '{"firstname"}', '')
+      || ' ' || coalesce(state_ #>> '{"lastname"}', '')
+      || ' ' || coalesce(state_ #>> '{"email"}', '')
+      || ' ' || coalesce(state_ #>> '{"phone"}', '')
+      || ' ' || coalesce(state_ #>> '{"extension"}', '')
+      || ' ' || coalesce(state_ #>> '{"department"}', '')
+      || ' ' || coalesce(state_ #>> '{"position"}', '')
+      || ' ' || coalesce(state_ #>> '{"organization"}', '')
+      || ' ' || coalesce(state_ #>> '{"location"}', '')
+      || ' ' || coalesce(state_ #>> '{"country"}', '')
+      || ' ' || coalesce(state_ #>> '{"website"}', '')
+      || ' ' || coalesce(state_ #>> '{"languages"}', '')
+      || ' ' || coalesce(state_ #>> '{"office"}', '')
+      || ' ' || coalesce(state_ #>> '{"room_no"}', '')
+      || ' ' || coalesce(state_ #>> '{"biography"}', '');
+  elseif class_name = 'karl.content.models.files.CommunityFile' then
+    hoid := state_ #>> '{"_extracted_data", "id", 0}';
     if hoid is not null then
       select cls, state
       from object_json where lpad(to_hex(zoid), 16, '0'::text) = hoid
-      into class_name, state;
+      into class_name, state_;
       if class_name != 'karl.content.models.adapters._CachedData' then
         raise 'bad data in CommunityFile % %', hoid, class_name;
       end if;
-      return content_text(class_name, state);
+      return content_text(class_name, state_);
     end if;
     text := '';
   else
-    text := coalesce(state #>> '{"text"}', '');
+    text := coalesce(state_ #>> '{"text"}', '');
   end if;
 
   textv := to_tsvector(text);
 
-  if state ? 'title' then
+  if state_ ? 'title' then
     textv := textv
-      || setweight(to_tsvector(state #>> '{"title"}'), 'A')
-      || setweight(to_tsvector(coalesce(state #>> '{"description"}', '')), 'B');
+      || setweight(to_tsvector(state_ #>> '{"title"}'), 'A')
+      || setweight(to_tsvector(coalesce(state_ #>> '{"description"}', '')), 'B');
   else
     textv := textv
-      || setweight(to_tsvector(coalesce(state #>> '{"description"}', '')), 'A');
+      || setweight(to_tsvector(coalesce(state_ #>> '{"description"}', '')), 'A');
   end if;
 
   return textv;
